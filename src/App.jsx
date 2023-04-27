ae1fb92031e46a96963e453ac3d39d046984f965
Alejo <alejobuc@gmail.com>
2023-04-27 14:28:40 -0300

    Notes V:2.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import './App.css'
 import notesArr from './assets/notes.jsx'
 import axios from "axios"
 import noteService from './services/data.jsx'
+import Loader from './loader.jsx'
 
 function App() {
+  const [home, setHome] = useState(true)
   const [notes, setNotes] = useState("")
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
   const [deleteNote, setDeleteNote] = useState('')
+  const [loader, setLoader] = useState(false)
 
   useEffect(() => {
     noteService
       .GetAll()
       .then(notes => {
         setNotes(notes)
+        setLoader(true)
       })
   }, [])
 
@@ -50,10 +54,7 @@ function App() {
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
-  const handleDeleteNote = (event) => {
-    setDeleteNote(event.target.value)
-  }
-  const handleValue = (event) => {
+  const handleDelete = (event) => {
     setDeleteNote(event.target.id)
   }
 
@@ -64,47 +65,103 @@ function App() {
     noteService.Update(id, changedNote)
   }
 
+  const classLifunction = (note) => {
+    if (note.important) {
+      if (deleteNote === note._id) {
+        return 'li li-important li-select'
+      }
+      return 'li li-important'
+    }
+    if (deleteNote === note._id) {
+      return 'li li-select'
+    }
+    return 'li'
+  }
+
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
+  const handleHome = () => {
+    if (home) {
+      return (
+        <div className="Home" >
+          <h1>Notes</h1>
+          <p>This is an application made with React.js and MongoDB about a crud of Notes</p>
+          <p style={{
+            fontSize: '22px',
+            fontWeight: 'bold'
+          }}>(may take a while to load)</p>
+          <button onClick={() => setHome(false)} className='btn-home'>
+            Start
+          </button>
+        </div >
+      )
+    } else {
+      return (
+        <>
+          {
+            loader ?
+              <div className="App">
+                <div className='title'>
+                  <h1>Notes</h1>
+                  <div>
+                    <button style={{
+                      marginLeft: '0px',
+                    }} onClick={() => setShowAll(!showAll)}>
+                      show {showAll ? 'important' : 'all'}
+                    </button>
+                  </div>
+                </div>
+                <ul>
+                  {
+                    notesToShow.map(note => {
+                      const label = note.important ? 'make not important' : 'make important'
+                      const classLi = classLifunction(note)
+                      return (
+                        <li key={note._id} className={classLi} >
+                          <div onClick={handleDelete} id={note._id}>{note.content}</div>
+                          <button onClick={() => toggleImportance(note._id)}>{label}</button>
+                        </li>
+                      )
+                    })}
+                </ul>
+                <form onSubmit={addNote}>
+                  <input value={newNote} onChange={handleNoteChange} type="text" />
+                  <button className='btn-add' type="submit">Add note</button>
+                </form>
+                <div>
+                  <button onClick={deleteNoteFunction} className='btn-delete'>
+                    Delete note
+                  </button>
+                  <p className='commentDelete'>click on a note to delete</p>
+                </div>
+                <div>
+                  <button onClick={() => setHome(true)} className='btn-home btn-home-main'>
+                    Home
+                  </button>
+                </div>
+              </div>
+              : <Loader />
+          }
+        </>
+      )
+    }
+  }
+  const footerClass = home ? 'footer' : 'footer-main'
   return (
     <>
-      {
-        notes != "" &&
-        <div className="App">
-          <h1>Notes</h1>
-          <div>
-            <button style={{
-              marginLeft: '0px',
-            }}  onClick={() => setShowAll(!showAll)}>
-              show {showAll ? 'important' : 'all'}
-            </button>
-          </div>
-          <ul>
-            {
-              notesToShow.map(note => {
-                console.log(notesToShow);
-                const label = note.important ? 'make not important' : 'make important'
-                return (
-                  <li key={note._id} className='li'>
-                    <div onClick={handleValue} id={note._id}>{note.content}</div>
-                    <button onClick={() => toggleImportance(note._id)}>{label}</button>
-                  </li>
-                )
-              })}
-          </ul>
-          <form onSubmit={addNote}>
-            <input value={newNote} onChange={handleNoteChange} type="text" />
-            <button type="submit">Add note</button>
-          </form>
-          <form onSubmit={deleteNoteFunction} className='formdelete'>
-            <input type="text" value={deleteNote} onChange={handleDeleteNote} readOnly />
-            <button type="submit">Delete note</button>
-            <p className='deleteP'>Click in note to copy ID</p>
-          </form>
+      <div className='MainAPP'>
+        {handleHome()}
+      </div>
+      <footer className={footerClass}>
+        <p>Hecho y diseñado por <a href="https://www.linkedin.com/in/alejo-lacroix/" target="_blank" rel="noreferrer">Alejo Lacroix</a> </p>
+        <div className="social">
+          <a target="_blank" href="https://github.com/alejolac/Notes-Front-End"><i className="fa-brands fa-github"></i></a>
+          <a target="_blank" href="https://www.linkedin.com/in/alejo-lacroix/"><i className="fa-brands fa-linkedin"></i></a>
         </div>
-      }
+      </footer>
     </>
   )
 }
+
 
 export default App
---
+++ src/loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+    return (
+        <>
+            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+        </>
+    )
+}
+
+export default Loader
